7d1e7106cf7599ec1b1dd4b0d0c77b398d0af5fc
blue dream <aryan.it08@gmail.com>
2020-04-30 22:58:27 +0600

    initial ui

---
+++ app/src/main/java/com/rapples/arafat/ctos/Customer_signup.kt
@@ -0,0 +1,12 @@
+package com.rapples.arafat.ctos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Customer_signup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_signup)
+    }
+}
---
+++ app/src/main/java/com/rapples/arafat/ctos/Seller_signup.kt
@@ -0,0 +1,12 @@
+package com.rapples.arafat.ctos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Seller_signup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_signup)
+    }
+}
